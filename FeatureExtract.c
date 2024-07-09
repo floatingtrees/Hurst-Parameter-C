e3d7b500098cecca0084837fb112049027aff030
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2024-07-09 12:39:38 -0700

    stuff works now

--- FeatureExtract.c
+++ FeatureExtract.c
@@ -5,67 +5,82 @@
 #include <stdbool.h>
 #include <assert.h>
 
-
-typedef struct {
+typedef struct
+{
     double packet;
     double time;
 } TimeSeries;
 
-typedef struct {
+typedef struct
+{
     double RS;
     double size;
 } DataPoint;
 
-double calculateSubarrayMean(const TimeSeries* ts, int startIndex, int endIndex) {
+double calculateSubarrayMean(const TimeSeries *ts, int startIndex, int endIndex)
+{
     // values include [start, end)
-    if (startIndex >= endIndex) {
+    if (startIndex >= endIndex)
+    {
         printf("Wrong Start and End Indexes");
         exit(2);
     }
     double mean = 0;
 
-    for (int i = startIndex; i < endIndex; ++i) {
+    for (int i = startIndex; i < endIndex; ++i)
+    {
         mean += ts[i].packet;
     }
     mean = mean / (endIndex - startIndex);
     return mean;
 }
 
-
-bool checkFlawed(double value) {
-    if (isnan(value)) {
+bool checkFlawed(double value)
+{
+    if (isnan(value))
+    {
         return true;
-    } else if (isinf(value)) {
+    }
+    else if (isinf(value))
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 // Function to read CSV file and store it in a TimeSeries array
-int readCSV(const char* file_time_name, const char* file_packet_name, TimeSeries* ts, int max_size) {
+int readCSV(const char *file_time_name, const char *file_packet_name, TimeSeries *ts, int max_size)
+{
     FILE *file_time = fopen(file_time_name, "r");
     FILE *file_packet = fopen(file_packet_name, "r");
-    if (!file_time || !file_packet) {
+    if (!file_time || !file_packet)
+    {
         printf("File opening failed.\n");
         exit(1);
     }
-    
+
     int time_counter = 0;
-    for (int i = 0; i < max_size; i++) {
+    for (int i = 0; i < max_size; i++)
+    {
         TimeSeries placeholder;
-        int counter1  = fscanf(file_time, "%lf", &placeholder.time);
+        int counter1 = fscanf(file_time, "%lf", &placeholder.time);
         int counter2 = fscanf(file_packet, "%lf", &placeholder.packet);
-        int result = placeholder.time/0.01;
-        if (result == time_counter) {
+        int result = placeholder.time / 0.1;
+        if (result == time_counter)
+        {
             ts[time_counter].packet += placeholder.packet;
         }
-        else{
+        else
+        {
             time_counter = result;
             ts[time_counter] = placeholder;
         }
 
-        if (counter1 != 1 || counter2 != 1) {
+        if (counter1 != 1 || counter2 != 1)
+        {
             printf("Terminating at %d\n", i);
             fclose(file_time);
             fclose(file_packet);
@@ -79,9 +94,11 @@ int readCSV(const char* file_time_name, const char* file_packet_name, TimeSeries
     return max_size;
 }
 
-double calculateSlope(DataPoint* datapoints, int start, int array_size) {
+double calculateSlope(DataPoint *datapoints, int start, int array_size)
+{
     double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
-    for (int i = start; i < array_size; i++) {
+    for (int i = start; i < array_size; i++)
+    {
         sumX += datapoints[i].size;
         sumY += datapoints[i].RS;
         sumXY += datapoints[i].size * datapoints[i].RS;
@@ -90,118 +107,138 @@ double calculateSlope(DataPoint* datapoints, int start, int array_size) {
 
     double numerator = array_size * sumXY - sumX * sumY;
     double denominator = array_size * sumX2 - sumX * sumX;
-    
-    if (denominator == 0) {
+
+    if (denominator == 0)
+    {
         fprintf(stderr, "Error: Denominator is zero, slope is undefined.\n");
         return 0; // Handling division by zero, could also exit the program
     }
     double hurst = numerator / denominator;
-    if (hurst < 0) {
+    if (hurst < 0)
+    {
         printf("%lf, %lf, %lf, %lf", sumX, sumY, sumXY, sumX2);
         exit(1);
     }
     return hurst;
 }
 
-double calculateHurst(const TimeSeries* ts, int segment_size, int location, int array_size) {
-    
+double calculateHurst(const TimeSeries *ts, int segment_size, int location, int array_size)
+{
+
     int i = location; // location represents start location
     int endTime = segment_size + ts[location].time;
     int count = 0;
-    
-    while (i < array_size) {
-        if (ts[location + i].time > endTime) {
+
+    while (i < array_size)
+    {
+        if (ts[location + i].time > endTime)
+        {
             break;
         }
-        else {
+        else
+        {
             ++count;
         }
         ++i;
     }
-    //printf("COUNT IS %d\n", count);
-    
+    // printf("COUNT IS %d\n", count);
+
     const int L = count / 4; // automatically casts to integer division
-    if (L < 4) return -1; // deal with this edge case later
+    if (L < 4)
+        return -1; // deal with this edge case later
 
     DataPoint dataset[L]; // first few points are filler
 
-    for (int k = 4; k < L; ++k) { // k = num_segments
+    for (int k = 4; k < L; ++k)
+    { // k = num_segments
         const int L2 = count / k;
         double means[k];
         double stds[k];
         double ranges[k];
         int j = 0;
         // if k == 4, divide array into 4 segments
-        for (i = 0; i < k; ++i){ // calculate the mean 
-            means[i] = calculateSubarrayMean(ts, i * L2, (i+1) * L2);
+        for (i = 0; i < k; ++i)
+        { // calculate the mean
+            means[i] = calculateSubarrayMean(ts, i * L2, (i + 1) * L2);
         }
         int index = 0;
         double mean_centered_value;
-        
-        for (i = 0; i < k; ++i) { // calculate ranges, std
-        // CHECK IF RANGE IS CALCULATED CORRECTLY
+
+        for (i = 0; i < k; ++i)
+        { // calculate ranges, std
+            // CHECK IF RANGE IS CALCULATED CORRECTLY
             double std_counter = 0;
 
             double highest = ts[index].packet - means[i];
             double lowest = ts[index].packet - means[i];
             double centered_value;
-            for (j = 0; j < L2; ++j) {
+            for (j = 0; j < L2; ++j)
+            {
                 centered_value = ts[index].packet - means[i];
-                if (centered_value < lowest) {
+                if (centered_value < lowest)
+                {
                     lowest = centered_value;
                 }
-                else if (centered_value > highest) {
+                else if (centered_value > highest)
+                {
                     highest = centered_value;
                 }
                 mean_centered_value = centered_value;
-                std_counter = std_counter +  (mean_centered_value * mean_centered_value);
+                std_counter = std_counter + (mean_centered_value * mean_centered_value);
                 ++index;
                 // INDEX INCREMENTORS IS WORKING
-                //printf("%d, %d, %d, %d\n", index, j + i * L2 + 1, i, k);
+                // printf("%d, %d, %d, %d\n", index, j + i * L2 + 1, i, k);
                 assert(index == j + i * L2 + 1);
                 assert(index <= array_size);
-                //printf("%lf, %lf, %lf\n", lowest, highest, means[i]);
+                // printf("%lf, %lf, %lf\n", lowest, highest, means[i]);
             }
-            
+
             std_counter = std_counter / L2;
             stds[i] = sqrt(std_counter);
             ranges[i] = highest - lowest;
-            //printf("%lf, %lf, %lf, %d\n", highest, lowest, stds[i], L2);
+            // printf("%lf, %lf, %lf, %d\n", highest, lowest, stds[i], L2);
         }
         double ratios = 0;
         int counter2 = 0;
-        for (i = 0; i < k; ++i) {
-            if (stds[i] < 0.00000000001 || ranges[i] < 0.00000000001) {
+        for (i = 0; i < k; ++i)
+        {
+            if (stds[i] < 0.00000000001 || ranges[i] < 0.00000000001)
+            {
                 continue;
             }
             ratios += ranges[i] / stds[i];
             // std consistently lower than range
             counter2++;
         }
-        //printf("%lf", ratios / (double)counter2);
-        // take the mean of ratios
+        // printf("%lf", ratios / (double)counter2);
+        //  take the mean of ratios
         dataset[k].RS = log10(ratios / (double)counter2);
-        dataset[k].size = log10((double) L2); // It's either k or L2
-        if (checkFlawed(dataset[k].RS) || checkFlawed(dataset[k].size)) {
+        dataset[k].size = log10((double)L2); // It's either k or L2
+        if (checkFlawed(dataset[k].RS) || checkFlawed(dataset[k].size))
+        {
             printf("\n%lf, %lf,  %d, %d", dataset[k].RS, dataset[k].size, k, counter2);
             exit(1);
         }
-
     }
 
     double hurst = calculateSlope(dataset, 4, L);
-    // sum_y doesn't work 
+    // sum_y doesn't work
     return hurst;
 }
 
-void writeToCSV(double* hursts, int size, const char* filename) {
+void writeToCSV(double *hursts, int size, const char *filename)
+{
     FILE *file = fopen(filename, "w");
 
-    for (int i = 0; i < size * 3; i++) {
-        fprintf(file, "%.2f", hursts[i]);  // Writing the double value with two decimal places
-        if (i % 3 == 2) {  // After every third element, start a new line
+    for (int i = 0; i < size * 3; i++)
+    {
+        fprintf(file, "%.2f", hursts[i]); // Writing the double value with two decimal places
+        if (i % 3 == 2)
+        { // After every third element, start a new line
             fprintf(file, "\n");
-        } else if (i != size - 1) {  // Place a comma if it's not the last element in a row or the last element overall
+        }
+        else if (i != size - 1)
+        { // Place a comma if it's not the last element in a row or the last element overall
             fprintf(file, ",");
         }
     }
@@ -209,33 +246,39 @@ void writeToCSV(double* hursts, int size, const char* filename) {
     fclose(file);
 }
 
-int main() {
+int main()
+{
     int segment_sizes[3] = {10, 30, 60};
     // Allocating memory for a very large array
-    TimeSeries* ts = malloc(50331648 * sizeof(TimeSeries)); // Corrected to sizeof(TimeSeries)
-    if (ts == NULL) {
+    TimeSeries *ts = malloc(50331648 * sizeof(TimeSeries)); // Corrected to sizeof(TimeSeries)
+    exit(1);
+    if (ts == NULL)
+    {
         printf("Memory allocation failed.\n");
         return 1; // Return an error code indicating failure
     }
-   
+
     int size = readCSV("Name_Time_VideoInjection_tester", "Name_Packet_VideoInjection_tester", ts, 50331648); // Correct filename extension and variable name
 
-    double* hursts = malloc(3 * size * sizeof(double));
-    if (hursts == NULL) {
+    double *hursts = malloc(3 * size * sizeof(double));
+    if (hursts == NULL)
+    {
         printf("Memory allocation failed.\n");
         return 1; // Return an error code indicating failure
     }
-    //#pragma omp parallel for
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 3; j++) {
+    // #pragma omp parallel for
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             // Accessing the element at row i, column j
             // CHANGE THE PLUS ONE AFTER DEBUGGING
             // VERY VERY IMPORTANT
-            hursts[i * 3 + j] = calculateHurst(ts, segment_sizes[j], i, size);  // Assign a value to each element
-            
+            hursts[i * 3 + j] = calculateHurst(ts, segment_sizes[j], i, size); // Assign a value to each element
         }
-        if (i % 1 == 0) {
-        printf("DONE CALCULATING %d out of %d, %d, %lf \n", i, size, omp_get_thread_num(), hursts[i]);
+        if (i % 1 == 0)
+        {
+            printf("DONE CALCULATING %d out of %d, %d, %lf \n", i, size, omp_get_thread_num(), hursts[i]);
         }
     }
     printf("HIHIHI");
--- FeatureExtract2.c
+++ FeatureExtract2.c
@@ -5,36 +5,42 @@
 #include <stdbool.h>
 #include <assert.h>
 
-typedef struct {
+typedef struct
+{
     double packet;
     double time;
 } TimeSeries;
 
-typedef struct {
+typedef struct
+{
     double RS;
     double size;
 } DataPoint;
 
-
-double calculateSubarrayMean(const TimeSeries* ts, int startIndex, int endIndex) {
+double calculateSubarrayMean(const TimeSeries *ts, int startIndex, int endIndex)
+{
     // values include [start, end)
-    if (startIndex >= endIndex) {
+    if (startIndex >= endIndex)
+    {
         printf("%d, %d\n", startIndex, endIndex);
         printf("Wrong Start and End Indexes");
         exit(2);
     }
     double mean = 0;
 
-    for (int i = startIndex; i < endIndex; ++i) {
+    for (int i = startIndex; i < endIndex; ++i)
+    {
         mean += ts[i].packet;
     }
     mean = mean / (endIndex - startIndex);
     return mean;
 }
 
-double calculateSlope(const DataPoint* datapoints, int start, int array_size) {
+double calculateSlope(const DataPoint *datapoints, int start, int array_size)
+{
     double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
-    for (int i = start; i < array_size; i++) {
+    for (int i = start; i < array_size; i++)
+    {
         sumX += datapoints[i].size;
         sumY += datapoints[i].RS;
         sumXY += datapoints[i].size * datapoints[i].RS;
@@ -44,20 +50,25 @@ double calculateSlope(const DataPoint* datapoints, int start, int array_size) {
     double numerator = (array_size - start) * sumXY - sumX * sumY;
     double denominator = (array_size - start) * sumX2 - sumX * sumX;
 
-    if (denominator == 0) {
-        fprintf(stderr, "Error: Denominator is zero, slope is undefined.\n");
-        for (int i = start; i < array_size; i++) {
+    if (denominator == 0)
+    {
+        for (int i = start; i < array_size; i++)
+        {
             printf("%lf", datapoints[i].size);
         }
+        fprintf(stderr, "Error: Denominator is zero, slope is undefined.\n");
+        exit(1);
         return 0; // Handling division by zero, could also exit the program
     }
 
     return numerator / denominator;
 }
 
-double calculateStandardDeviation(const TimeSeries datapoints[], int start, int array_size, double meanRS) {
+double calculateStandardDeviation(const TimeSeries datapoints[], int start, int array_size, double meanRS)
+{
     double sumSqDiff = 0;
-    for (int i = start; i < array_size; i++) {
+    for (int i = start; i < array_size; i++)
+    {
         double diff = datapoints[i].packet - meanRS;
         sumSqDiff += diff * diff;
     }
@@ -65,21 +76,26 @@ double calculateStandardDeviation(const TimeSeries datapoints[], int start, int 
     return sqrt(sumSqDiff / (array_size - start));
 }
 
-double calculateRange(const TimeSeries datapoints[], int start_index, int end_index) {
-    if (start_index > end_index) {
+double calculateRange(const TimeSeries datapoints[], int start_index, int end_index)
+{
+    if (start_index > end_index)
+    {
         fprintf(stderr, "Error: start_index cannot be greater than end_index.\n");
         return 0; // Return 0 or appropriate error code/value
     }
 
     double minRS = datapoints[start_index].packet;
     double maxRS = datapoints[start_index].packet;
-    
+
     // Traverse the array from start_index to end_index
-    for (int i = start_index; i < end_index; i++) {
-        if (datapoints[i].packet < minRS) {
+    for (int i = start_index; i < end_index; i++)
+    {
+        if (datapoints[i].packet < minRS)
+        {
             minRS = datapoints[i].packet;
         }
-        if (datapoints[i].packet > maxRS) {
+        if (datapoints[i].packet > maxRS)
+        {
             maxRS = datapoints[i].packet;
         }
     }
@@ -87,65 +103,78 @@ double calculateRange(const TimeSeries datapoints[], int start_index, int end_in
     return maxRS - minRS; // Return the range
 }
 
-
-
-bool checkFlawed(double value) {
-    if (isnan(value)) {
+bool checkFlawed(double value)
+{
+    if (isnan(value))
+    {
         return true;
-    } else if (isinf(value)) {
+    }
+    else if (isinf(value))
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 // Function to read CSV file and store it in a TimeSeries array
-int readCSV(const char* file_time_name, const char* file_packet_name, TimeSeries* ts, int max_size) {
+int readCSV(const char *file_time_name, const char *file_packet_name, TimeSeries *ts, int max_size)
+{
     FILE *file_time = fopen(file_time_name, "r");
     FILE *file_packet = fopen(file_packet_name, "r");
-    if (!file_time || !file_packet) {
+    if (!file_time || !file_packet)
+    {
         printf("File opening failed.\n");
         exit(1);
     }
-    
-    int time_counter = 0;
-    for (int i = 0; i < max_size; i++) {
-        TimeSeries placeholder;
-        int counter1  = fscanf(file_time, "%lf", &placeholder.time);
-        int counter2 = fscanf(file_packet, "%lf", &placeholder.packet);
-        int result = placeholder.time/0.01;
-        if (result == time_counter) {
-            ts[time_counter].packet += placeholder.packet;
-        }
-        else{
-            time_counter = result;
-            ts[time_counter] = placeholder;
-        }
 
-        if (counter1 != 1 || counter2 != 1) {
-            printf("Terminating at %d\n", i);
+    int time_counter = 0;
+    for (int i = 0; i < max_size; i++)
+    {
+        TimeSeries placeholder;
+        int counter1 = fscanf(file_time, "%lf", &placeholder.time);
+        int counter2 = fscanf(file_packet, "%lf", &placeholder.packet);
+        int result = placeholder.time / 0.1;
+        if (counter1 != 1 || counter2 != 1)
+        {
+
+            printf("Terminating at %d\n", time_counter);
             fclose(file_time);
             fclose(file_packet);
-            return i;
+            return time_counter;
+        }
+        if (result == time_counter)
+        {
+            ts[time_counter].packet += placeholder.packet;
+        }
+        else
+        {
+            time_counter = result;
+            ts[time_counter] = placeholder;
         }
     }
     max_size = time_counter;
 
     fclose(file_time);
     fclose(file_packet);
-    return max_size;
+    return time_counter;
 }
 
-
-
-void writeToCSV(double* hursts, int size, const char* filename) {
+void writeToCSV(double *hursts, int size, const char *filename)
+{
     FILE *file = fopen(filename, "w");
 
-    for (int i = 0; i < size * 3; i++) {
-        fprintf(file, "%.2f", hursts[i]);  // Writing the double value with two decimal places
-        if (i % 3 == 2) {  // After every third element, start a new line
+    for (int i = 0; i < size * 3; i++)
+    {
+        fprintf(file, "%.2f", hursts[i]); // Writing the double value with two decimal places
+        if (i % 3 == 2)
+        { // After every third element, start a new line
             fprintf(file, "\n");
-        } else if (i != size - 1) {  // Place a comma if it's not the last element in a row or the last element overall
+        }
+        else if (i != size - 1)
+        { // Place a comma if it's not the last element in a row or the last element overall
             fprintf(file, ",");
         }
     }
@@ -153,39 +182,48 @@ void writeToCSV(double* hursts, int size, const char* filename) {
     fclose(file);
 }
 
-double calculateHurst(const TimeSeries* ts, int segment_size, int location, int array_size) {
+double calculateHurst(const TimeSeries *ts, int segment_size, int location, int array_size)
+{
     int i = location;
     int count = 0;
     int endTime = segment_size + ts[location].time;
-    while (i < array_size) {
-        if (ts[location + i].time > endTime) {
+    while (i < array_size)
+    {
+        if (ts[location + i].time > endTime)
+        {
             break;
         }
-        else {
+        else
+        {
             ++count;
         }
         ++i;
     }
     int L = count / 4;
-    if (count <= 10) return 0;
+    if (L <= 5)
+        return -1;
     // k is the length of each individual segment
     DataPoint dataset[L - 4];
-    for (int k = 4; k < L; ++k) {
+    for (int k = 4; k < L; ++k)
+    {
         int num_segments = count / k;
-        double* means= malloc(num_segments * sizeof(double));
-        double* stds = malloc(num_segments * sizeof(double));
-        double* ranges = malloc(num_segments * sizeof(double));
+        double *means = malloc(num_segments * sizeof(double));
+        double *stds = malloc(num_segments * sizeof(double));
+        double *ranges = malloc(num_segments * sizeof(double));
 
-        for (int j = 0; j < num_segments; ++j) {
-            means[j] = calculateSubarrayMean(ts, j * k, (j + 1)* k);
+        for (int j = 0; j < num_segments; ++j)
+        {
+            means[j] = calculateSubarrayMean(ts, j * k, (j + 1) * k);
             stds[j] = calculateStandardDeviation(ts, j * k, (j + 1) * k, means[j]);
-            ranges[j] = calculateRange(ts, j * k, (j + 1)* k);
+            ranges[j] = calculateRange(ts, j * k, (j + 1) * k);
         }
 
         double ratios = 0;
         int nonzero_counter = 0;
-        for (int j = 0; j < num_segments; ++j) {
-            if (stds[j] <= 0) {
+        for (int j = 0; j < num_segments; ++j)
+        {
+            if (stds[j] <= 0)
+            {
                 continue;
             }
             ratios += ranges[j] / stds[j];
@@ -196,40 +234,54 @@ double calculateHurst(const TimeSeries* ts, int segment_size, int location, int 
         dataset[k - 4].size = log10(k);
     }
     double hurst = calculateSlope(dataset, 0, L - 4);
-    if (hurst < 0 | hurst > 1 | checkFlawed(hurst)) {
+    if (hurst < 0 || hurst > 1 || checkFlawed(hurst))
+    {
         printf("%lf", hurst);
         exit(1);
     }
     return hurst;
 }
 
-
-int main() {
+int main()
+{
     int segment_sizes[3] = {10, 30, 60};
     // Allocating memory for a very large array
-    TimeSeries* ts = malloc(50331648 * sizeof(TimeSeries)); // Corrected to sizeof(TimeSeries)
-    if (ts == NULL) {
+    TimeSeries *ts = malloc(50331648 * sizeof(TimeSeries)); // Corrected to sizeof(TimeSeries)
+    for (int i = 0; i < 50331648; ++i)
+    {
+        ts[i].packet = 0;
+        ts[i].time = 0;
+    }
+    if (ts == NULL)
+    {
         printf("Memory allocation failed.\n");
         return 1; // Return an error code indicating failure
     }
-   
+
     int size = readCSV("Name_Time_VideoInjection_tester", "Name_Packet_VideoInjection_tester", ts, 50331648); // Correct filename extension and variable name
 
-    double* hursts = malloc(3 * size * sizeof(double));
-    if (hursts == NULL) {
+    double *hursts = malloc(3 * size * sizeof(double));
+    if (hursts == NULL)
+    {
         printf("Memory allocation failed.\n");
         return 1; // Return an error code indicating failure
     }
     int i;
     int j;
-    #pragma omp parallel for num_threads(4)
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < 3; j++) {
-            hursts[i * 3 + j] = calculateHurst(ts, segment_sizes[j], i, size);  // Assign a value to each element
-            
+    // #pragma omp parallel for num_threads(4)
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            hursts[i * 3 + j] = calculateHurst(ts, segment_sizes[j], i, size); // Assign a value to each element
         }
-        if (i % 1 == 0) {
-        printf("DONE CALCULATING %d out of %d, %d, %lf \n", i, size, omp_get_thread_num(), hursts[i]);
+        if (i % 100 == 0)
+        {
+            printf("DONE CALCULATING %d out of %d, %d, %lf \n", i, size, omp_get_num_threads(), hursts[i]);
+        }
+        if (i == 1000)
+        {
+            exit(1);
         }
     }
     writeToCSV(hursts, size, "outputs.csv");
